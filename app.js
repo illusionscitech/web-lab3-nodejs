971916665e0055e8794304233a0670b78041c538
梦幻科技 <836976779@qq.com>
2023-11-14 17:38:23 +0700

    现在前端可以接收到后端的数据了，添加了HTML文件
    Now the front end can receive data from the back end

--- app.js
+++ app.js
@@ -1,56 +1,5 @@
-// //将blog回调函数引入
-// const handleBlogRoute = require('./src/routes/blog')
-// //写回到函数，将createServer中的回调函数抽离了出来，目的是更好的维护
-// const serverHandler = async (req, res) => {
-//     //这里面是服务器的业务代码
-
-//     // 处理请求路径
-//     const url = req.url;
-//     req.path = url.split('?')[0]; // 问号分割取前半段
-        
-//     //1.设置返回的数据类型
-//     res.setHeader('Content-Type', 'application/json')
-//         //路由返回数据结果(将请求和响应传入)
-//         const blogData = await handleBlogRoute(req, res)
-//         if (blogData) {
-//             //如果拿到了，就结束请求，返回一个响应
-//             res.end(
-//                 JSON.stringify(blogData)
-//             );
-//             //响应之后，下面的代码不再执行，所以return一下
-//             return;
-//         }
-
-//         // // 根据 req.path 进行其他路由的判断
-//         // if (req.path === '/api/xxx') {
-//         //     // 处理 /api/xxx 路由的逻辑
-//         //     res.end(JSON.stringify({ message: 'Handling /api/xxx route' }));
-//         //     return;
-//         // }
-//         //如果不是上面的路由，就会返回下面的结果
-//         res.writeHead(404, { 'Content-Type': 'text/plain' }); //无格式正文
-//         res.write('404 Not Found');
-//         res.end(); //终止响应
-//     // //2.数据格式
-//     // const responseData = {
-//     //     name: 'alva',
-//     //     age: 21
-//     // }
- 
-//     // res.end(
-//     //     JSON.stringify(responseData)
-//     // )
-// }
- 
-// module.exports = serverHandler;
-
 const querystring = require('querystring')
 const handleBlogRoute = require('./src/routes/blog')
-const express = require('express');
-const app = express();
- 
-// 使用 JSON 解析器中间件
-app.use(express.json());
 
 //处理post数据
 const getPostData = (req) => {
--- bin/www.js
+++ bin/www.js
@@ -1,11 +1,47 @@
 //创建服务器
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
  
 //将服务器回调函数引入
 const serverHandler = require('../app')
+
+const server = http.createServer(async (req, res) => {
+    // 处理静态文件
+    // if (req.url.startsWith('/public/Blog_page.html')) {
+    //     const filePath = path.join(__dirname, req.url);
+    //     fs.readFile(filePath, (err, data) => {
+    //         if (err) {
+    //             res.writeHead(404, { 'Content-Type': 'text/plain' });
+    //             res.write('404 Not Found');
+    //             res.end();
+    //         } else {
+    //             res.writeHead(200, { 'Content-Type': 'text/html' });
+    //             res.write(data);
+    //             res.end();
+    //         }
+    //     });
+    if (req.url === '/' || req.url === '/public/Blog_page.html') {
+        // 读取并返回 HTML 文件内容
+        const filePath = path.join(__dirname,'..', 'public','Blog_page.html');
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+                return;
+            }
+
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        });
+    } else {
+        // 处理其他请求
+        await serverHandler(req, res);
+    }
+});
+
  
- 
-const server = http.createServer(serverHandler);
+// const server = http.createServer(serverHandler);
  
 // server.listen(PORT, () => {
 //     // 监听成功执行的回调
